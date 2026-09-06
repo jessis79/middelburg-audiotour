c26b1aa3903c699ba4ab558841b7a0278534847f
jessis79 <jessica-muller@hotmail.com>
2026-09-06 16:10:19 +0200

    Add Mijn Balans offline support

---
+++ mijn-balans/sw.js
@@ -0,0 +1,5 @@
+const CACHE='mijn-balans-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('mijn-balans')).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));

c26f34e43612032b1df86ed68781ee5614fd0a10
jessis79 <jessica-muller@hotmail.com>
2026-09-06 16:26:15 +0200

    Refresh Mijn Balans cache for barcode scanner update

--- mijn-balans/sw.js
+++ mijn-balans/sw.js
@@ -1,5 +1,5 @@
-const CACHE='mijn-balans-v2';
+const CACHE='mijn-balans-v4';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('mijn-balans')).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('mijn-balans')&&k!==CACHE).map(k=>caches.delete(k))))]))});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin)caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))})
